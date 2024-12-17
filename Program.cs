4dcdc5852e03e9a78fe6536941c8bc8260465d1b
FeridAbbaszade <f.abbasov2001@gmail.com>
2024-12-17 19:37:35 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 24;
+            if (a % 4 == 0 || a % 5 == 0)
+            {
+                Console.WriteLine("bolunur");
+
+            }
+            else { Console.WriteLine("bolunmur"); }
+        }
+
+    }
+}

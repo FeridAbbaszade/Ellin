e980edf2316dbebb3b88b223353c287e71a7adfd
FeridAbbaszade <f.abbasov2001@gmail.com>
2024-12-17 21:21:08 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿namespace tasklar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region task start
+            //task1
+            //int a = 24;
+            //if (a % 4 == 0 || a % 5 == 0) {
+            //    Console.WriteLine("bolunur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("bolunmur");
+            //}
+            //task2
+            //int grade = 40;
+            //if(grade>=91 && grade <= 100)
+            //{
+            //    Console.WriteLine("a")
+            //}
+            //else if(grade>=81 && grade<91)
+            //{
+            //    Console.WriteLine("b")
+            //}
+            //else if (grade>=71 && grade<81)
+            //{
+            //    Console.WriteLine("c")  
+            //}
+            //else if((grade>=61 && grade<71)
+            //    {
+            //    Console.WriteLine("d")
+            //}
+            //else if (grade>=51 && grade<61)
+            //{
+            //    Console.WriteLine("e")  
+            //}
+            //else if((grade<51)
+            //    {
+            //    Console.WriteLine("f")
+            //}
+            //else
+            //{  Console.WriteLine("hec biri aid deyil")}
+            //task3
+            //int grade = 40;
+
+            //switch (grade)
+            //{
+            //    case 1 
+            //         if(grade >= 91 && n <= 100):
+            //        Console.WriteLine("a");
+            //        break;
+            //    case 2
+            //        if(grade >= 81 && n < 91):
+            //        Console.WriteLine("b");
+            //        break;
+            //    case 3
+            //        if(grade >= 71 && n < 81):
+            //        Console.WriteLine("c");
+            //        break;
+            //    case 4
+            //        if(grade >= 61 && n < 71):
+            //        Console.WriteLine("d");
+            //        break;
+            //    case 5 
+            //        if(grade >= 51 && n < 61):
+            //        Console.WriteLine("e");
+            //        break;
+            //    case 6
+            //        if(grade < 51):
+            //        Console.WriteLine("f");
+            //        break;
+            //    default:
+            //        Console.WriteLine("heç biri aid deyil");
+            //        break;
+            //}}
+
+            //task4
+            //int a = 97;
+            //int teklik = a % 10;
+            //int onluq = (a - teklik) / 10;
+            //Console.WriteLine(onluq); Console.WriteLine(teklik);
+            //task5
+            //int a = 97;
+            //int teklik = a % 10;
+            //int onluq = (a - teklik) / 10;
+            //int result = teklik * 10 + onluq;
+            //Console.WriteLine(result);
+            //task6
+            //int a = 253;
+            //int teklik= a % 10;
+            //int onluq=((a%100)-teklik) / 10;
+            //int yuzluk = (a - (a % 100)) / 100;
+            //Console.WriteLine(yuzluk);
+            //Console.WriteLine(onluq);
+            //Console.WriteLine(teklik);
+            //task7
+        //    int a = 5783;
+        //    int teklik = a % 10;
+        //    int onluq = ((a % 100) - teklik) / 10;
+        //    int yuzluk = (a - (a % 100)) / 100;
+        //    int minlik = a / 1000;
+        //    Console.WriteLine(minlik);
+        //    Console.WriteLine(yuzluk);
+        //    Console.WriteLine(onluq);
+        //    Console.WriteLine(teklik);
+        //}
+
+            //task8
+            //int a = 34;
+            //int b = 42;
+            //int c = 53;
+            //int d = 47;
+            //int e = 32;
+            //int result=(a + b + c + d + e)/5;
+            //Console.WriteLine(result);
+            //task9
+            //int a = 34;
+            //int b = 42;
+            //int c = 53;
+            //int d = 47;
+            //int e = 32;
+            //int result = (a + b + c + d + e) / 5;
+            //if(result%3=1)
+            //    Console.WriteLine("bolunmur");
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //#region task end
+
+
+         
